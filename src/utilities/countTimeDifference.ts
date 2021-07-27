24dd7a1cfb68159762b669ea392cbcb8ddc4e936
Kazakova-A <arina.kazakova.dev@gmail.com>
2021-07-27 14:32:05 +0300

    fix cost canculation

--- src/utilities/countTimeDifference.ts
+++ src/utilities/countTimeDifference.ts
@@ -6,10 +6,10 @@ export default (
   type?: 'days' | 'months',
 ) => {
   const diff = moment(endDate * 1000).diff(startDate * 1000);
-  const diffInDays =
+  const difference =
     type === 'days'
-      ? moment.duration(diff).days()
+      ? moment.duration(diff).asDays()
       : moment.duration(diff).months();
 
-  return Math.abs(diffInDays);
+  return Math.abs(difference);
 };
